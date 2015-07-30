0ae8a1ca67b48368cd1dfe09db0bbfc266a2d8a5
swaaa <mike@von-dem-brinke.de>
2015-07-30 12:04:02 +0200

    create Fake Thumbnails

--- app/src/main/java/com/example/android/popular_movies_3rd/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popular_movies_3rd/MainActivityFragment.java
@@ -18,6 +18,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Integer[] mThumbnailIds = {
+                R.mipmap.ic_launcher, R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher, R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher, R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher, R.mipmap.ic_launcher,
+        }
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
