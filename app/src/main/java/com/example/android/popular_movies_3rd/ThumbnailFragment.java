a49ffd8089d07a9b4a8fc77b1b6480292aa98d47
swaaa <mike@von-dem-brinke.de>
2015-07-30 15:40:07 +0200

    inflate fragment menu

--- app/src/main/java/com/example/android/popular_movies_3rd/ThumbnailFragment.java
+++ app/src/main/java/com/example/android/popular_movies_3rd/ThumbnailFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -43,6 +45,11 @@ public class ThumbnailFragment extends Fragment {
         gridView.setAdapter(imageAdapter);
 
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment, menu);
     }
 
     public class FetchMoviesTask extends AsyncTask<Void, Void, Void> {
